458c9d97df9aa16be4b55b51baf2c7ac880ed2af
NiyazFair <niyaz048@gmail.com>
2024-05-28 09:29:43 +0300

    Add files via upload

---
+++ module 2 2.py
@@ -0,0 +1,21 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second == third:
+    print(3)
+else:
+    print(0)
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second or second == third or first ==third:
+    print(2)
+else:
+    print(5)
+first = int(input())
+second = int(input())
+third = int(input())
+if first != second or second != third or first !=third:
+    print(0)
+else:
+    print(1)
